151d0f8128d1a41bac7581e8c0879494e6d1db66
SQAKelompok5DTI <76054118+SQAKelompok5DTI@users.noreply.github.com>
2020-12-17 20:28:31 +0700

    Add files via upload

--- shared-objects/id.js
+++ shared-objects/id.js
@@ -1,4 +1,12 @@
-module.export = {
-    email: "yusufsurya00@gmail.com",
-    pw : "Kmzway19"
+module.exports = {
+    NamaLengkap : "Kelompok 5" ,
+    NamaLengkapBaru : "DTI Kelompok 5",
+    emailDaftar : "SQA01kelompok5@gmail.com" ,
+    emailLama : "kelompok5dti@gmail.com" ,
+    nomor :"08221833486",
+    nomorBaru : "081234567891",
+    sandi : "harrypotter100%",
+    sandiBaru : "harrypotter",
+    konfirmasisandi :  "harrypotter100%",
+    konfirmasisandiBaru : "harrypotter",
 }
