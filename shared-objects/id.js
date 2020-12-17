984515f2815e3771bb36640b02b415606f23d661
SQAKelompok5DTI <76054118+SQAKelompok5DTI@users.noreply.github.com>
2020-12-17 20:39:09 +0700

    Update id.js

--- shared-objects/id.js
+++ shared-objects/id.js
@@ -1,12 +1,14 @@
 module.exports = {
-    NamaLengkap : "Kelompok 5" ,  // signup case
-    NamaLengkapBaru : "DTI Kelompok 5", // signup case
-    emailDaftar : "SQA01kelompok5@gmail.com" , // signup case
-    emailLama : "kelompok5dti@gmail.com" , // signup case
-    nomor :"08221833486", // signup case
-    nomorBaru : "081234567891", // signup case
-    sandi : "harrypotter100%", // signup case
-    sandiBaru : "harrypotter", // signup case
-    konfirmasisandi :  "harrypotter100%", // signup case
-    konfirmasisandiBaru : "harrypotter", // signup case
+    NamaLengkap : "Kelompok 5" ,  // signup feature
+    NamaLengkapBaru : "DTI Kelompok 5", // signup feature
+    emailDaftar : "SQA01kelompok5@gmail.com" , // signup feature
+    emailLama : "kelompok5dti@gmail.com" , // signup feature
+    nomor :"08221833486", // signup feature
+    nomorBaru : "081234567891", // signup feature
+    sandi : "harrypotter100%", // signup feature
+    sandiBaru : "harrypotter", // signup feature
+    konfirmasisandi :  "harrypotter100%", // signup feature
+    konfirmasisandiBaru : "harrypotter", // signup feature
+    email : " ", //tambah riwayat feature
+    pw : " "//tambah riwayat feature
 }
