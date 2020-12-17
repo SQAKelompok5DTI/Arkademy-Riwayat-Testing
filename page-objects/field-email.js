e54797ff740e033bc8be51f4aa9ae015cfd91716
SQAKelompok5DTI <76054118+SQAKelompok5DTI@users.noreply.github.com>
2020-12-17 21:26:50 +0700

    Testing Fitur Login

---
+++ page-objects/field-email.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    url: 'https://sso.arkademy.com/auth/realms/production/protocol/openid-connect/auth?client_id=arkademy-production-client&scope=openid%20email&response_type=code&redirect_uri=https%3A%2F%2Fservices.arkademy.com%2Fauth%2Flogin%2Fverify%3Ftoken%3D18d7c6c3ee5fc4123836e25adbacbfbd&nonce=141eae42-19d5-4081-a20a-e87bd615fe71&client_name=arkademy-prod',
+
+    elements: {
+        fieldEmail: by.xpath("//input[@id='username']")
+    },
+
+    performFill: function () {
+
+        var selector = page.fieldEmail.elements.fieldEmail;
+        return driver.findElement(selector).sendKeys(shared.id.email);
+    }
+};
